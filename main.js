92a785c649bdebcc0089d99f47b98618dcdde8b8
Mitchell Stahl <mitchellstahl@Mitchells-MBP.attlocal.net>
2022-07-23 18:06:37 -0400

    mobile-optimization-4

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-const accordionItemHeaders = document.querySelectorAll('.accordion__item__header');
+const accordionItemHeaders = document.querySelectorAll(
+  ".accordion__item__header"
+);
 
-accordionItemHeaders.forEach(accordionItemHeader => {
-    accordionItemHeader.addEventListener('click', event => {
-        accordionItemHeader.classList.toggle('active');
-    })
-})
+accordionItemHeaders.forEach((accordionItemHeader) => {
+  accordionItemHeader.addEventListener("click", (event) => {
+    accordionItemHeader.classList.toggle("active");
+  });
+});
+
+const options = {};
+
+const swiperView = document.querySelector(".third");
+const observer = new IntersectionObserver(function (entries, observer) {
+  entries.forEach((entry) => {
+    console.log(entry.isIntersecting);
+    if (entry.isIntersecting) {
+      swiper.autoplay.start();
+      console.log("AUTOPLAY");
+    } else {
+      swiper.autoplay.stop();
+      console.log("NO AUTOPLAY");
+    }
+  });
+}, options);
+
+observer.observe(swiperView);
