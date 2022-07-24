82659ffd6d3b9dcc0e10f80d536a67f5fd1b5ba3
Mitchell Stahl <mitchellstahl@Mitchells-MBP.attlocal.net>
2022-07-24 02:32:42 -0400

    mobile-final-3

--- main.js
+++ main.js
@@ -25,3 +25,40 @@ const observer = new IntersectionObserver(function (entries, observer) {
 }, options);
 
 observer.observe(swiperView);
+
+const hamburger = document.querySelector(".hamburger");
+const mobileNav = document.querySelector(".mobile-nav");
+
+let newWidth = document.documentElement.clientWidth;
+let newHeight = document.documentElement.clientHeight;
+
+hamburger.addEventListener("click", function () {
+  mobileNav.classList.toggle("is-active");
+  hamburger.classList.toggle("is-active");
+});
+
+let logo = document.querySelector("#logo");
+let group = document.querySelector(".group");
+let huge = document.querySelector(".huge");
+
+window.addEventListener("load", function (window) {
+  loadWidth = document.documentElement.clientWidth;
+
+  if (newWidth > 935) {
+    logo.src = "6FLMainLogo.svg";
+  } else {
+    logo.src = "LogoWhite.svg";
+  }
+});
+
+window.addEventListener("resize", function (window) {
+  newWidth = document.documentElement.clientWidth;
+  newHeight = document.documentElement.clientHeight;
+  if (newWidth > 935) {
+    mobileNav.classList.remove("is-active");
+    hamburger.classList.remove("is-active");
+    logo.src = "6FLMainLogo.svg";
+  } else {
+    logo.src = "LogoWhite.svg";
+  }
+});
