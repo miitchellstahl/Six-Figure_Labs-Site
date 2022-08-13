7c9e1c0ec30cefc3afcf09abc0387078b52ff53b
Mitchell Stahl <mitchellstahl@Mitchells-MacBook-Pro.local>
2022-08-13 01:45:07 -0400

    circles

--- nav.js
+++ nav.js
@@ -1,8 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
 const mobileNav = document.querySelector(".mobile-nav");
+const desktopNav = document.querySelector(".desktop-nav");
+const groupHeader = document.querySelector(".header");
 
 let newWidth = document.documentElement.clientWidth;
 let newHeight = document.documentElement.clientHeight;
+
+const navOptions = {};
 
 hamburger.addEventListener("click", function () {
   mobileNav.classList.toggle("is-active");
@@ -17,24 +21,66 @@ window.addEventListener("load", function (window) {
   loadWidth = document.documentElement.clientWidth;
 
   if (newWidth > 935) {
-    logo.src = "6FLMainLogo.svg";
     group.classList.remove("shadow");
   } else {
-    logo.src = "LogoWhite.svg";
     group.classList.add("shadow");
+  }
+
+  if (pageYOffset > 35) {
+    groupHeader.classList.add("nav-scrolled");
+    groupHeader.classList.add("shadow");
+  } else {
+    groupHeader.classList.remove("nav-scrolled");
+    groupHeader.classList.remove("shadow");
+  }
+
+  if (newWidth < 935) {
+    // alert(newWidth);
+    groupHeader.classList.remove("nav-scrolled");
+    groupHeader.classList.remove("shadow");
   }
 });
 
 window.addEventListener("resize", function (window) {
   newWidth = document.documentElement.clientWidth;
   newHeight = document.documentElement.clientHeight;
+  console.log(newHeight);
+  console.log(newWidth);
   if (newWidth > 935) {
     mobileNav.classList.remove("is-active");
     hamburger.classList.remove("is-active");
     group.classList.remove("shadow");
-    logo.src = "6FLMainLogo.svg";
   } else {
-    logo.src = "LogoWhite.svg";
     group.classList.add("shadow");
+  }
+
+  if (pageYOffset > 35) {
+    groupHeader.classList.add("nav-scrolled");
+    groupHeader.classList.add("shadow");
+  } else {
+    groupHeader.classList.remove("nav-scrolled");
+    groupHeader.classList.remove("shadow");
+  }
+
+  if (newWidth < 935) {
+    // alert(newWidth);
+    groupHeader.classList.remove("nav-scrolled");
+    groupHeader.classList.remove("shadow");
+  }
+});
+window.addEventListener("scroll", function () {
+  console.log(pageYOffset);
+  if (pageYOffset > 35) {
+    groupHeader.classList.add("nav-scrolled");
+    groupHeader.classList.add("shadow");
+  } else {
+    groupHeader.classList.remove("nav-scrolled");
+    groupHeader.classList.remove("shadow");
+  }
+
+  if (newWidth < 935) {
+    // alert(newWidth);
+    groupHeader.classList.remove("nav-scrolled");
+    groupHeader.classList.remove("shadow");
   }
 });
